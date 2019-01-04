124f0e91792a7fa8b6e80b1e7eb38219634313a1
Tyler Ferrara <tylerferrara123@gmail.com>
2019-01-03 19:56:24 -0500

    fixed enviornment var reference

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "qs": { "access_token": global_access_token },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
